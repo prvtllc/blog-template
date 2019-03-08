86d9e1797a37f4dbf73b6f2cd9c612ba3ab3a3f8
Domingo Rivera <djr@infosecusa.com>
2019-03-08 18:56:13 -0500

    Revert "Update config.js"
    This reverts commit 23a175862a83d4ef7ac9d86a0238416e9dca9ddd.

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   url: 'https://cybertrialattorney.netlify.com',
-  title: 'Cyber Trial Attorney Domingo J Rivera',
-  subtitle: 'Technology consulting, Internet defamation, cyber security and forensics.',
-  copyright: '© 2012-2019. All rights reserved.',
+  title: 'Blog by John Doe',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -23,14 +23,16 @@ module.exports = {
     }
   ],
   author: {
-    name: 'Domingo Rivera',
+    name: 'John Doe',
     photo: '/photo.jpg',
-    bio: 'Internet law expert and cybersecurity / digital forensics consultant.',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
-      email: 'djr@4n6va.com',
-      twitter: 'domingojrivera',
+      email: '#',
+      telegram: '#',
+      twitter: '#',
       github: '#',
       rss: '#',
+      vkontakte: '#'
     }
   }
 };
