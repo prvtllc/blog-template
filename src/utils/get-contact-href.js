7c18a8d8c8723d482628cf6d9a28ea9415c8c910
Domingo Rivera <djr@infosecusa.com>
2019-03-08 19:42:37 -0500

    Update get-contact-href.js

--- src/utils/get-contact-href.js
+++ src/utils/get-contact-href.js
@@ -3,19 +3,19 @@ const getContactHref = (name, contact) => {
 
   switch (name) {
     case 'twitter':
-      href = `https://www.twitter.com/${contact}`;
+      href = `https://www.twitter.com/domingojrivera${contact}`;
       break;
     case 'github':
-      href = `https://github.com/${contact}`;
+      href = `https://www.icyberlaw.com/${contact}`;
       break;
     case 'vkontakte':
-      href = `https://vk.com/${contact}`;
+      href = `https://www.cyberinternetlawyer.com/${contact}`;
       break;
     case 'telegram':
-      href = `telegram:${contact}`;
+      href = `https://www.infosecusa.com${contact}`;
       break;
     case 'email':
-      href = `mailto:${contact}`;
+      href = `mailto:djr@4n6va.com${contact}`;
       break;
     default:
       href = contact;
